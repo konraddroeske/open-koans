c45abe952ac86c9bac01d4e41cf7b26208fa7344
Konrad Droeske <kdroeske@gmail.com>
2023-06-02 10:11:21 -0400

    initial commit

---
+++ src/main/java/com/openkoans/openkoans/OpenKoansApplication.java
@@ -0,0 +1,13 @@
+package com.openkoans.openkoans;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OpenKoansApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OpenKoansApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/openkoans/openkoans/OpenKoansApplicationTests.java
@@ -0,0 +1,13 @@
+package com.openkoans.openkoans;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OpenKoansApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
