5dcff49bc427abe8f019df684352e43e3ccf0c10
Konrad Droeske <kdroeske@gmail.com>
2023-06-04 07:44:01 -0400

    updated seed data

--- src/main/resources/db/migration/data/V2__Seed_data.sql
+++ src/main/resources/db/migration/data/V2__Seed_data.sql
@@ -1,5 +1,5 @@
 INSERT INTO users (username, password, email)
-VALUES ('Konrad', 'test1', 'test1@test.com');
+VALUES ('Bob', 'test1', 'test1@test.com');
 
 INSERT INTO users (username, password, email)
-VALUES ('Nancy', 'test2', 'test2@test.com');
+VALUES ('Sue', 'test2', 'test2@test.com');
